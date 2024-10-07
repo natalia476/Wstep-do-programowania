3aa06e6925bbf824afc1c211255235f99c74e263
NataliaCharz <nat.charzynska@gmail.com>
2024-10-07 20:13:05 +0200

    "First commit"

---
+++ Lab1.py
@@ -0,0 +1,111 @@
+# zad1
+print("Hello world")
+
+#zad2
+print("    ^")
+print("   ^^^")
+print("  ^^^^^")
+print(" ^^^^^^^")
+print("  ^^^^^")
+print(" ^^^^^^^")
+print("^^^^^^^^^")
+print("   ###")
+
+#zad3
+x=7
+print(x)
+
+#zad4
+a=7
+b=5
+print(a)
+print(a+3)
+print(a*3)
+print(a+b)
+print(a/b)
+print("Suma a i b to: ", a+b)
+print("a=", a, ", b=", b)
+c = b-a
+print(c)
+
+#zad5
+a=2
+b=3
+c = a*a
+d=b*b
+e=c+d
+print("Liczba a= ", a, "; liczba b=", b)
+print("Suma kwadratów tych liczb to ", e)
+
+#zad6
+a=4
+h=2
+p=(a*h)/2
+print(p)
+
+#zad7
+a=2
+b=3
+p=a*b
+obw=(2*a)+(2*b)
+print(p)
+print(obw)
+
+#zad8
+znaki = "abc hej!"
+print("Znaki to: ", znaki)
+
+#zad9
+a=3
+print("Gdy do ", a, " dodasz 1, to wyjdzie ", a+1)
+print("Gdy do ", a, " dodasz 1, to wyjdzie ", a+1, sep="")
+print("Gdy do ", a, " dodasz 1, to wyjdzie ", a+1, sep="-")
+
+#zad10
+bombka = "o"
+print("    ^")
+print("   ^^^")
+print("  ^",bombka,"^^^")
+print(" ^^^^^",bombka,"^")
+print("  ^^^^^")
+print(" ^^",bombka,"^^^^")
+print("^^^^^^", bombka,"^^")
+print("   ###")
+
+#zad11
+import math
+a = 16
+b = math.sqrt(a)
+print(b)
+
+print(math.pi)
+print(math.sin(0))
+print(math.sin(math.pi/2))
+print(math.floor(2.3))
+
+#zad12
+a=2
+pole=a*a
+obwod=4*a
+przekatna=math.sqrt(2)*a
+print(pole)
+print(obwod)
+print(przekatna)
+
+#zad13
+a = 180
+b = math.radians(a)
+print(b)
+
+d=1
+c = math.degrees(d)
+print(c)
+
+#zad14
+a=4
+b=3
+alfa=30
+pole=(a*b*math.sin(alfa))/2
+print(pole)
+
+#zad15
