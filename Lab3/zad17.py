df7768f6685d4fbcf3b92e0fc69ba1a1f1f9d0a1
NataliaCharz <nat.charzynska@gmail.com>
2024-10-22 12:34:58 +0200

    Laboratorium 3

--- Lab3/zad17.py
+++ Lab3/zad17.py
@@ -4,8 +4,7 @@ iloscLiczbPierwszych = 0
 
 for i in range(2, 101):
     isPrime = True
-    n = math.sqrt(i)
-    for j in range(2, int(n + 1)):
+    for j in range(2, i):
         if i % j == 0:
             isPrime = False
             break
